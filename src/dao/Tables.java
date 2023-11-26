c5caefef29b54fd3876ac68ddde73e8d5bad8244
Aswin-K08 <aswinkala1999@gmail.com>
2023-11-26 23:30:55 +0530

    Update Tables.java

--- src/dao/Tables.java
+++ src/dao/Tables.java
@@ -12,8 +12,9 @@ public class Tables {
         try{
         Connection con = ConnectionProvider.getCon();
         Statement st =con.createStatement();
-        st.executeUpdate("create table appuser(appuser_pk int AUTO_INCREMENT primary key,userRole varchar(200),name varchar(200),dob varchar(50),mobilenumber varchar(50),email varchar(200),username varchar(200),password varchar(200),address varchar(200))");
-        st.executeUpdate("insert into appuser(userRole,name,dob,mobileNumber,email,username,password,address)values('Admin','Admin','16-12-1992','956565656','admindin@gmail.com','admin','admin','India')");
+        //st.executeUpdate("create table appuser(appuser_pk int AUTO_INCREMENT primary key,userRole varchar(200),name varchar(200),dob varchar(50),mobilenumber varchar(50),email varchar(200),username varchar(200),password varchar(200),address varchar(200))");
+       // st.executeUpdate("insert into appuser(userRole,name,dob,mobileNumber,email,username,password,address)values('Admin','Admin','16-12-1992','956565656','admindin@gmail.com','admin','admin','India')");
+       st.executeUpdate("create table medicine(medicine_pk int AUTO_INCREMENT primary key,uniqueId varchar(200),name varchar(200),companyName varchar(200),quantity bigint,price bigint)");
         JOptionPane.showMessageDialog(null,"Table Created Successfully");
         }
         catch(Exception e){
