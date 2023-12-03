0b7f2483bf6626bdfdb0e0681098ec21d79b323e
Akhil-Joseph-2203 <akhiljoseph12c@gmail.com>
2023-12-03 13:22:56 +0530

    One more

--- src/AdminDashboard.java
+++ src/AdminDashboard.java
@@ -174,22 +174,22 @@ public static void main(String args[]) {
 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Dashboard.class  
+            java.util.logging.Logger.getLogger(AdminDashboard.class  
 
 .getName()).log(java.util.logging.Level.SEVERE, null, ex);
 
 } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Dashboard.class  
+            java.util.logging.Logger.getLogger(AdminDashboard.class  
 
 .getName()).log(java.util.logging.Level.SEVERE, null, ex);
 
 } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Dashboard.class  
+            java.util.logging.Logger.getLogger(AdminDashboard.class  
 
 .getName()).log(java.util.logging.Level.SEVERE, null, ex);
 
 } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Dashboard.class  
+            java.util.logging.Logger.getLogger(AdminDashboard.class  
 
 .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
